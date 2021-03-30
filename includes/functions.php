3a0fb20d45b1ecbba69f0dc82d3970bfb49428f0
Mike Hemberger <mike@thestizmedia.com>
2021-03-30 11:38:54 -0400

    add mapped sync

--- includes/functions.php
+++ includes/functions.php
@@ -7,12 +7,13 @@
  *
  * @return
  */
-add_action( 'user_register',  'maiup_sync_user_post', 99 );
-add_action( 'profile_update', 'maiup_sync_user_post', 99 );
-add_action( 'delete_user',    'maiup_delete_user_post' );
+add_action( 'user_register',      'maiup_sync_user_post', 99 );
+add_action( 'profile_update',     'maiup_sync_user_post', 99 );
+add_action( 'edit_post_mai_user', 'maiup_sync_post_user', 99, 2 );
+add_action( 'delete_user',        'maiup_delete_user_post' );
 
 /**
- * Get meta keys to sync.
+ * Gets meta keys to sync.
  *
  * @since 0.1.0
  *
@@ -31,7 +32,7 @@ function maiup_get_meta_keys() {
 }
 
 /**
- * Gets ACF keys to sync.
+ * Gets ACF meta keys to sync.
  *
  * @since 0.1.0
  *
@@ -50,6 +51,31 @@ function maiup_get_acf_keys() {
 }
 
 /**
+ * Gets mapped keys to sync.
+ * Useful when keys are different from user to post meta.
+ * Array of user key => post key.
+ *
+ * [
+ *   'user_image_id' => '_thumbnail_id',
+ * ]
+ *
+ * @since 0.1.0
+ *
+ * @return array
+ */
+function maiup_get_mapped_keys() {
+	static $mapped_keys = null;
+
+	if ( ! is_null( $mapped_keys ) ) {
+		return $mapped_keys;
+	}
+
+	$mapped_keys = apply_filters( 'maiup_mapped_keys', [] );
+
+	return (array) $mapped_keys;
+}
+
+/**
  * Syncs user data to the user post
  * when a user is registered or profile is updated.
  *
@@ -63,8 +89,8 @@ function maiup_sync_user_post( $user_id ) {
 	$user_roles = maiup_get_user_roles();
 
 	if ( $user_roles ) {
-		$user_meta  = get_userdata( $user_id );
-		$has_role   = (bool) array_intersect( $user_meta->roles, $user_roles );
+		$user_meta = get_userdata( $user_id );
+		$has_role  = (bool) array_intersect( $user_meta->roles, $user_roles );
 
 		if ( ! $has_role ) {
 			return;
@@ -77,27 +103,32 @@ function maiup_sync_user_post( $user_id ) {
 		return;
 	}
 
-	$post = maiup_get_user_post( $user_id );
+	$post_id = 0;
+	$post    = maiup_get_user_post( $user_id );
 
 	if ( $post ) {
 		$post_id = $post->ID;
 	} else {
 		$post_id = maiup_create_user_post( $user_id );
+	}
 
-		if ( ! $post_id ) {
-			return;
-		}
+	if ( ! $post_id ) {
+		return;
 	}
 
 	$user = get_user_by( 'id', $user_id );
+
+	remove_action( 'edit_post_mai_user', 'maiup_sync_post_user', 99, 2 );
 
 	wp_update_post(
 		[
 			'ID'           => $post_id,
 			'post_title'   => $user->display_name,
-			'post_content' => $user->description,
+			'post_excerpt' => $user->description,
 		]
 	);
+
+	add_action( 'edit_post_mai_user', 'maiup_sync_post_user', 99, 2 );
 
 	$meta_keys = maiup_get_meta_keys();
 
@@ -129,9 +160,139 @@ function maiup_sync_user_post( $user_id ) {
 		}
 	}
 
+	$mapped_keys = maiup_get_mapped_keys();
+
+	if ( $mapped_keys ) {
+		foreach ( $mapped_keys as $user_key => $post_key ) {
+			$user_meta = get_user_meta( $user_id, $user_key, true );
+			$post_meta = get_post_meta( $post_id, $post_key, true );
+
+			if ( $user_meta === $post_meta ) {
+				continue;
+			}
+
+			update_post_meta( $post_id, $post_key, $user_meta );
+		}
+	}
+
 	do_action( 'maiup_user_updated', $user_id, $post_id );
 }
 
+/**
+ * Syncs user post to the user
+ * when a user post is updated.
+ *
+ * @since 0.1.0
+ *
+ * @param int     $post_id The post ID.
+ * @param WP_Post $post    The post object.
+ *
+ * @return void
+ */
+function maiup_sync_post_user( $post_id, $post ) {
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if ( 'trash' === $post->post_status ) {
+		return;
+	}
+
+	$user_id = get_post_meta( $post_id, 'mai_user_id', true );
+
+	if ( ! $user_id ) {
+		return;
+	}
+
+	$user = get_user_by( 'id', $user_id );
+
+	if ( ! $user ) {
+		return;
+	}
+
+	$user_roles = maiup_get_user_roles();
+
+	if ( $user_roles ) {
+		$user_meta = get_userdata( $user_id );
+		$has_role  = (bool) array_intersect( $user_meta->roles, $user_roles );
+
+		if ( ! $has_role ) {
+			return;
+		}
+	}
+
+	$should_sync = apply_filters( 'maiup_should_sync', true, $user_id );
+
+	if ( ! $should_sync ) {
+		return;
+	}
+
+	$post = get_post( $post_id );
+
+	remove_action( 'user_register', 'maiup_sync_user_post', 99 );
+	remove_action( 'profile_update', 'maiup_sync_user_post', 99 );
+
+	wp_update_user(
+		[
+			'ID'           => $user_id,
+			'user_url'     => get_post_meta( $post_id, 'user_url', true ),
+			'display_name' => $post->post_title,
+			'description'  => $post->post_excerpt,
+		]
+	);
+
+	add_action( 'user_register', 'maiup_sync_user_post', 99 );
+	add_action( 'profile_update', 'maiup_sync_user_post', 99 );
+
+	update_user_meta( $user_id, 'post_content', $post->post_content );
+
+	$meta_keys = maiup_get_meta_keys();
+
+	if ( $meta_keys ) {
+		foreach ( $meta_keys as $key ) {
+			$post_meta = get_post_meta( $post_id, $key, true );
+			$user_meta = get_user_meta( $user_id, $key, true );
+
+			if ( $post_meta === $user_meta ) {
+				continue;
+			}
+
+			update_user_meta( $user_id, $key, $post_meta );
+		}
+	}
+
+	$acf_keys = maiup_get_acf_keys();
+
+	if ( $acf_keys && function_exists( 'get_field' ) && function_exists( 'update_field' ) ) {
+		foreach ( $acf_keys as $key ) {
+			$post_meta = get_field( $key, $post_id );
+			$user_meta = get_field( $key, 'user_' .  $user_id );
+
+			if ( $post_meta === $user_meta ) {
+				continue;
+			}
+
+			update_field( $key, $post_meta, 'user_' .  $user_id );
+		}
+	}
+
+	$mapped_keys = maiup_get_mapped_keys();
+
+	if ( $mapped_keys ) {
+		foreach ( $mapped_keys as $user_key => $post_key ) {
+			$post_meta = get_post_meta( $post_id, $post_key, true );
+			$user_meta = get_user_meta( $user_id, $user_key, true );
+
+			if ( $post_meta === $user_meta ) {
+				continue;
+			}
+
+			update_user_meta( $user_id, $user_key, $post_meta );
+		}
+	}
+
+	do_action( 'maiup_user_post_updated', $user_id, $post_id );
+}
 
 /**
  * Creates a user post if it doesn't exist.
@@ -153,18 +314,24 @@ function maiup_create_user_post( $user_id ) {
 
 	if ( $user ) {
 
-		$args = apply_filters( 'maiup_user_post_args', [
+		$args = [
 			'post_type'    => 'mai_user',
 			'post_author'  => $user_id,
-			'post_content' => $user->description,
-			'post_name'    => maiup_get_user_slug( $user_id ),
+			'post_excerpt' => $user->description,
 			'post_status'  => 'publish',
 			'post_title'   => $user->display_name,
-		] );
+			'meta_input'   => [
+				'mai_user_id' => absint( $user_id ),
+			],
+		];
+
+		$args = apply_filters( 'maiup_user_post_args', $args, $user_id );
 
 		$post_id = wp_insert_post( $args );
 
 		if ( $post_id && ! is_wp_error( $post_id ) ) {
+			do_action( 'maiup_user_post_created', $user_id, $post_id );
+
 			return $post_id;
 		}
 	}
@@ -198,20 +365,26 @@ function maiup_delete_user_post( $user_id ) {
  * @return WP_User|null
  */
 function maiup_get_user_post( $user_id ) {
-	return get_page_by_path( maiup_get_user_slug( $user_id ), OBJECT, 'mai_user' );
-}
-
-/**
- * Gets a user slug by user ID.
- *
- * @since 0.1.0
- *
- * @param int $user_id The user ID.
- *
- * @return string
- */
-function maiup_get_user_slug( $user_id ) {
-	return sprintf( 'user-%s', $user_id );
+	$post  = null;
+	$query = new WP_Query(
+		[
+			'post_type'              => 'mai_user',
+			'meta_key'               => 'mai_user_id',
+			'meta_value'             => $user_id,
+			'posts_per_page'         => 1,
+			'no_found_rows'          => true,
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false,
+		]
+	);
+	if ( $query->have_posts() ) {
+		while ( $query->have_posts() ) : $query->the_post();
+			$post = get_post( get_the_ID() );
+			break;
+		endwhile;
+	}
+	wp_reset_postdata();
+	return $post;
 }
 
 /**
